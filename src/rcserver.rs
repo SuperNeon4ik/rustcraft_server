c93aa3fd669c47fd2eca3e3aa9f1d21425cf9ec3
Nazar <youtube@superneon4ik.me>
2024-08-07 23:42:27 +0300

    Reading packets

--- src/rcserver.rs
+++ src/rcserver.rs
@@ -1,7 +1,7 @@
-use std::net::TcpListener;
+use std::{io::{BufReader, Read}, net::{Shutdown, TcpListener, TcpStream}, thread};
 
 pub struct MinecraftServer {
-    address: String
+    address: String,
 }
 
 impl MinecraftServer {
@@ -14,19 +14,42 @@ impl MinecraftServer {
     pub fn start_listening(&self) -> std::io::Result<()> {
         let listener = TcpListener::bind(&self.address)?;
 
-        println!("Listening on {}:{}", listener.local_addr()?.ip(), listener.local_addr()?.port());
+        let server_address = listener.local_addr()?;
+
+        println!("Listening on {}:{}", server_address.ip(), server_address.port());
 
         for stream in listener.incoming() {
             match stream {
                 Ok(stream) => {
-                    println!("Received a connection: {}:{}", stream.peer_addr().unwrap().ip(), stream.peer_addr().unwrap().port());
-                    stream.shutdown(std::net::Shutdown::Both).unwrap();
+                    let address = stream.peer_addr().unwrap();
+                    println!("Received a connection: {}:{}", address.ip(), address.port());
+                    thread::spawn(move || { 
+                        handle_connection(stream);
+                    });
                 }
                 Err(e) => println!("Failed to read incoming stream: {}", e)
             }
         }
 
-        drop(listener);
         Ok(())
     }
+}
+
+fn handle_connection(stream: TcpStream) {
+    let address = stream.peer_addr().unwrap();
+
+    let mut buf = BufReader::new(stream.try_clone().unwrap());
+    loop {
+        let mut bytes:Vec<u8> = Vec::new();
+        match buf.read_to_end(&mut bytes) {
+            Ok(b)  => {
+                if b == 0 { break; }
+                println!("Received data ({} bytes): {}", bytes.len(), hex::encode(bytes));
+            }
+            Err(e) => println!("Error receiving data: {}", e)
+        }
+    }
+
+    println!("Client {}:{} dropped", address.ip(), address.port());
+    stream.shutdown(Shutdown::Both).unwrap();
 }
