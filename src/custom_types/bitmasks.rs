ae704c513ded42d6318b7e4330ff0622152fa26b
Nazar <youtube@superneon4ik.me>
2024-09-07 19:32:27 +0300

    Fix DisplayedSkinParts bitmask

--- src/custom_types/bitmasks.rs
+++ src/custom_types/bitmasks.rs
@@ -11,13 +11,13 @@ pub struct DisplayedSkinParts {
 impl DisplayedSkinParts {
     pub fn from_bitmask(byte: u8) -> Self {
         Self {
-            cape_enabled: (0x01 & byte) == byte,
-            jacket_enabled: (0x02 & byte) == byte,
-            left_sleeve_enabled: (0x04 & byte) == byte,
-            right_sleeve_enabled: (0x08 & byte) == byte,
-            left_pants_enabled: (0x10 & byte) == byte,
-            right_pants_enabled: (0x20 & byte) == byte,
-            hat_enabled: (0x40 & byte) == byte,
+            cape_enabled: (0x01 & byte) != byte,
+            jacket_enabled: (0x02 & byte) != byte,
+            left_sleeve_enabled: (0x04 & byte) != byte,
+            right_sleeve_enabled: (0x08 & byte) != byte,
+            left_pants_enabled: (0x10 & byte) != byte,
+            right_pants_enabled: (0x20 & byte) != byte,
+            hat_enabled: (0x40 & byte) != byte,
         }
     }
 }
